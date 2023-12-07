e817ea748da244f42d1672e09837d5f411f275e2
Cyrus Blake <cyrusblake89@gmail.com>
2023-12-07 00:29:48 -0500

    more changes

--- cyrus-blake-portfolio/src/App.jsx
+++ cyrus-blake-portfolio/src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import img1 from './assets/Screenshot 2023-12-06 004630.png'
 import img2 from './assets/Screenshot 2023-11-01 085753.png'
+import img3 from './assets/Screenshot 2023-12-07 002518.png'
 
 function App() {
   
@@ -13,19 +14,16 @@ function App() {
       <div className="card">
         <div>
           <img src={img1} className="images" alt=''></img>
-          
           <img src={img2} className="images" alt=''></img>
         </div>
         <div>
-          <img src={img1} className="images" alt=''></img>
-
+          <img src={img3} className="images" alt=''></img>
           <img src={img1} className="images" alt=''></img>
         </div>
         <div>
           <img src={img1} className="images" alt=''></img>
           <img src={img1} className="images" alt=''></img>
         </div>
-
         <p className="">
         Click Here To View All Projects On GitHub
         </p>
